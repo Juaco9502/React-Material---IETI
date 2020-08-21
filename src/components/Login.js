d5e1709cba59a097956859d837cb5c2fbe78e641
Juan Camilo <juaco9602@hotmail.com>
2020-08-21 13:58:36 -0500

    readme update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,94 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import './Login.css'
 
-export class Todo extends React.Component {
 
-    constructor(props) {
-        super(props);
+export class Login extends React.Component{
+
+    constructor(props){
+        super(props); 
+        this.state = {username:"", password:""};
+        localStorage.setItem('username', "juan");
+        localStorage.setItem('password', "123456");
+        this.handleUsername = this.handleUsername.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleUsername(e) {
+        this.setState({username: e.target.value});
     }
 
-    render() {
+    handlePassword(e){
+        this.setState({password: e.target.value})
+    }
+
+    handleSubmit(e){
+        console.log(this.state.username);
+        console.log(this.state.password);
+        if (this.state.username === localStorage.getItem("username") 
+        && this.state.password === localStorage.getItem("password")){
+            this.props.autenticate();
+            localStorage.setItem("isLoggedIn",true);
+        }
+        else alert("incorrect user or password")
+    }
+    render(){
         return (
-            <tr>
-                <td>{this.props.text}</td>
-                <td>{this.props.priority}</td>
-                <td>{this.props.dueDate.format('DD-MM-YYYY')}</td>
-            </tr>
+            <React.Fragment>
+                <CssBaseline />
+                <main className="layout">
+                    <Paper className="paper">
+                        <Avatar className="avatar">
+                            <LockIcon />
+                        </Avatar>
+                        <Typography variant="h2">Sign in</Typography>
+                        <form className="form">
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="email">Email Address</InputLabel>
+                                <Input 
+                                    id="email" 
+                                    name="email" 
+                                    autoComplete="email" 
+                                    autoFocus 
+                                    value={this.state.username} 
+                                    onChange={this.handleUsername}
+                                />
+                            </FormControl>
+                            <FormControl margin="normal" required fullWidth>
+                                <InputLabel htmlFor="password">Password</InputLabel>
+                                <Input
+                                    name="password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                    value={this.state.password} 
+                                    onChange={this.handlePassword}
+                                />
+                                
+                            </FormControl>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className="submit"
+                                onClick={this.handleSubmit}
+                            >
+                                Sign in
+                            </Button>
+                        </form>
+                    </Paper>
+                </main>
+            </React.Fragment>
         );
     }
 
